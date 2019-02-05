ee30e9994d5f0d5d339cdbfe189fee6b7939075e
lyokato <lyo.kato@gmail.com>
2019-02-05 09:46:43 +0900

    ble: Frame Handling

--- webauthnkit/src/main/kotlin/webauthnkit/core/ctap/ble/BLEFIDOService.kt
+++ webauthnkit/src/main/kotlin/webauthnkit/core/ctap/ble/BLEFIDOService.kt
@@ -4,13 +4,22 @@ import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.le.AdvertiseSettings
 import android.content.Context
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import webauthnkit.core.authenticator.internal.InternalAuthenticator
+import webauthnkit.core.ctap.CTAPCommandType
+import webauthnkit.core.ctap.CTAPStatusCode
 import webauthnkit.core.ctap.ble.frame.FrameBuffer
 import webauthnkit.core.ctap.ble.peripheral.*
 import webauthnkit.core.ctap.ble.peripheral.annotation.*
 import webauthnkit.core.util.WAKLogger
 
+@ExperimentalCoroutinesApi
+@ExperimentalUnsignedTypes
 class BLEFIDOService(
-    private val context: Context
+    private val context:       Context,
+    private val authenticator: InternalAuthenticator
 ) {
 
     companion object {
@@ -35,39 +44,120 @@ class BLEFIDOService(
     private fun handleCommand(command: BLECommandType, data: ByteArray) {
 
         when (command) {
-            BLECommandType.Cancel    -> { handleCancel()        }
-            BLECommandType.MSG       -> { handleMSG(data)       }
-            BLECommandType.Error     -> { handleError(data)     }
-            BLECommandType.KeepAlive -> { handleKeepAlive(data) }
-            BLECommandType.Ping      -> { handlePing(data)      }
+            BLECommandType.Cancel    -> { handleBLECancel()        }
+            BLECommandType.MSG       -> { handleBLEMSG(data)       }
+            BLECommandType.Error     -> { handleBLEError(data)     }
+            BLECommandType.KeepAlive -> { handleBLEKeepAlive(data) }
+            BLECommandType.Ping      -> { handleBLEPing(data)      }
         }
     }
 
-    private fun handleCancel() {
-        WAKLogger.d(TAG, "handleCancel")
+    private fun handleBLECancel() {
+        WAKLogger.d(TAG, "handleBLE: Cancel")
     }
 
-    private fun handleKeepAlive(value: ByteArray) {
-        WAKLogger.d(TAG, "handleKeepAlive")
+    private fun handleBLEKeepAlive(value: ByteArray) {
+        WAKLogger.d(TAG, "handleBLE: KeepAlive")
         WAKLogger.d(TAG, "should be authenticator -> client")
         // TODO better closing step
+        // closeByError(InvalidOperation)
         close()
     }
 
-    private fun handleMSG(value: ByteArray) {
-        WAKLogger.d(TAG, "handleMSG")
-
+    private fun handleBLEMSG(value: ByteArray) {
+        WAKLogger.d(TAG, "handleBLE: MSG")
+        if (value.size < 1) {
+            // closeByError(NoEnoughSize)
+            return
+        }
+        val command = value[0].toInt()
+        when (command) {
+            CTAPCommandType.MakeCredential.rawValue -> {
+                if (value.size < 2) {
+                    // closeByError(NoEnoughSize)
+                    return
+                }
+                handleCTAPMakeCredential(value.sliceArray(0..value.size))
+            }
+            CTAPCommandType.GetAssertion.rawValue -> {
+                if (value.size < 2) {
+                    // closeByError(NoEnoughSize)
+                    return
+                }
+                handleCTAPGetAssertion(value.sliceArray(0..value.size))
+            }
+            CTAPCommandType.GetNextAssertion.rawValue -> {
+                handleCTAPGetNextAssertion()
+            }
+            CTAPCommandType.ClientPIN.rawValue -> {
+                // unsupported
+                if (value.size < 2) {
+                    // closeByError(NoEnoughSize)
+                    return
+                }
+                handleCTAPClientPIN(value.sliceArray(0..value.size))
+            }
+            CTAPCommandType.GetInfo.rawValue -> {
+                handleCTAPGetInfo()
+            }
+            CTAPCommandType.Reset.rawValue -> {
+                handleCTAPReset()
+            }
+            else -> {
+                handleCTAPUnsupportedCommand()
+            }
+        }
     }
 
-    private fun handleError(value: ByteArray) {
-        WAKLogger.d(TAG, "handleError")
+    private fun handleBLEError(value: ByteArray) {
+        WAKLogger.d(TAG, "handleBLE: Error")
         WAKLogger.d(TAG, "should be authenticator -> client")
-        // TODO better closing step
+        // TODO closeByError(InvalidOperation)
         close()
     }
 
-    private fun handlePing(value: ByteArray) {
-        WAKLogger.d(TAG, "handlePing")
+    private fun handleBLEPing(value: ByteArray) {
+        WAKLogger.d(TAG, "handleBLE: Ping")
+    }
+
+    private fun handleCTAPMakeCredential(value: ByteArray) {
+        WAKLogger.d(TAG, "handleCTAP: MakeCredential")
+    }
+
+    private fun handleCTAPGetAssertion(value: ByteArray) {
+        WAKLogger.d(TAG, "handleCTAP: GetAssertion")
+
+    }
+
+    private fun handleCTAPGetInfo() {
+        WAKLogger.d(TAG, "handleCTAP: GetInfo")
+    }
+
+    private fun handleCTAPClientPIN(value: ByteArray) {
+        WAKLogger.d(TAG, "handleCTAP: ClientPIN")
+        WAKLogger.d(TAG, "This feature is not supported on this library. Better verification methods are supported on authenticator side.")
+        handleCTAPUnsupportedCommand()
+    }
+
+    private fun handleCTAPReset() {
+        WAKLogger.d(TAG, "handleCTAP: Reset")
+    }
+
+    private fun handleCTAPGetNextAssertion() {
+        WAKLogger.d(TAG, "handleCTAP: GetNextAssertion")
+        WAKLogger.d(TAG, "This feature is not supported on this library. 'Key Selection' is done on authenticator side.")
+        handleCTAPUnsupportedCommand()
+    }
+
+    private fun handleCTAPUnsupportedCommand() {
+        WAKLogger.d(TAG, "handleCTAP: Unsupported Command")
+        // TODO closeByError()
+    }
+
+    private fun closeByError(error: CTAPStatusCode) {
+        // send error packet as 'status' notification
+        // delay 10ms
+        // close()
     }
 
     private fun close() {
@@ -88,19 +178,24 @@ class BLEFIDOService(
             fun controlPoint(req: WriteRequest, res: WriteResponse) {
                 WAKLogger.d(TAG, "@Write: controlPoint")
 
-                if (frameBuffer.putFragment(req.value)) {
+                GlobalScope.launch {
 
-                    if (frameBuffer.isDone()) {
+                    if (frameBuffer.putFragment(req.value)) {
 
-                        handleCommand(frameBuffer.getCommand(), frameBuffer.getData())
+                        if (frameBuffer.isDone()) {
+
+                            handleCommand(frameBuffer.getCommand(), frameBuffer.getData())
+                            frameBuffer.clear()
+                        }
+
+                    } else {
                         frameBuffer.clear()
+                        // TODO more detailed error should be passed to 'close'
+                        close()
                     }
 
-                } else {
-                    frameBuffer.clear()
-                    // TODO more detailed error should be passed to 'close'
-                    close()
                 }
+
             }
 
             @OnRead("F1D0FFF1-DEAA-ECEE-B42F-C9BA7ED623BB")
